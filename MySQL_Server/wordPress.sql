fddd810b95a1f4b3ef7529d7b2e35a78fd469429
shanepikeCN <shane.pike@outlook.com>
2015-12-01 07:13:28 +0000

    Changed SQL queries

--- MySQL_Server/wordPress.sql
+++ MySQL_Server/wordPress.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE wordpress;`
+CREATE DATABASE wordpress;
 CREATE USER wordpressuser@localhost;
 SET PASSWORD for wordpressuser@localhost = PASSWORD("password");
 GRANT ALL PRIVILEGES on wordpress.* TO wordpressuser@localhost IDENTIFIED BY 'password';
